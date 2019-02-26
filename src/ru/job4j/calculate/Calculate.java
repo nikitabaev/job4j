cbb671e328a47a2c139036830518f1821601aa64
nikitabaev <nikobay@mail.ru>
2019-02-27 01:52:59 +0300

    1.1. Создание Hello World[#141]

---
+++ src/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,13 @@
+        
+    package ru.job4j.calculate;
+	
+              public class Calculate{
+				  
+			  public static void main(String[] args) {
+				  
+			  System.out.println("Hello World");
+			  
+			  }
+			  
+			   }
+													  
